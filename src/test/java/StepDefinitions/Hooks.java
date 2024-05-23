e4847d3351914c23776f52a9b2ea54088965c32a
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-05-23 20:19:04 +0200

    Hooks and Excel Utility are added

---
+++ src/test/java/StepDefinitions/Hooks.java
@@ -0,0 +1,32 @@
+package StepDefinitions;
+
+import Utilities.ExcelUtility;
+import Utilities.GWD;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+public class Hooks {
+    @Before// Cucumber ın Annotation ı
+    public void before(){
+        // System.out.println("Senaryo başladı");
+    }
+
+    @After // Cucumber ın Annotation ı
+    // her senaryodan sonra çalışır
+    public void after(Scenario senaryo){
+
+        ExcelUtility.writeToExcel("src/test/java/ApachePOI/resource/CucumberTestSonuclari.xlsx",
+                senaryo.getName()+" "+(senaryo.isFailed()? "Failed": "Passed") );
+
+        //senaryo fail olduysa ekran kaydı al
+        if (senaryo.isFailed()){
+            // aşağıdaki bölüm sadece extend report plugini devrede ise açılır
+//            TakesScreenshot ts=(TakesScreenshot) GWD.getDriver();
+//            byte[] hafizadakiHali= ts.getScreenshotAs(OutputType.BYTES);
+//            senaryo.attach(hafizadakiHali, "image/png", "screenshot name");
+        }
+
+        GWD.quitDriver();
+    }
+}
---
+++ src/test/java/Utilities/ExcelUtility.java
@@ -0,0 +1,79 @@
+package Utilities;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+
+public class ExcelUtility {
+    public static ArrayList<ArrayList<String>> getData(String path, String sheetName, int columnNumber) {
+        Sheet sheet = null;
+        try {
+            FileInputStream inputStream = new FileInputStream(path);
+            Workbook workbook = WorkbookFactory.create(inputStream);
+            sheet = workbook.getSheet(sheetName);
+        }
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
+
+        for (int i = 0; i <sheet.getPhysicalNumberOfRows(); i++) {
+            ArrayList<String> row = new ArrayList<>();
+            for (int j = 0; j < columnNumber; j++) {
+                row.add(sheet.getRow(i).getCell(j).toString() );
+            }
+            table.add(row);
+        }
+        return table;
+    }
+    public static void writeToExcel(String path, String testResult){
+        File file = new File(path);
+
+        if (!file.exists()){
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            XSSFSheet sheet = workbook.createSheet("Scenarios");
+
+            Row row = sheet.createRow(0);
+            Cell cell = row.createCell(0);
+            cell.setCellValue(testResult);
+
+            try{
+                FileOutputStream outputStream = new FileOutputStream(path);
+                workbook.write(outputStream);
+                workbook.close();
+                outputStream.close();
+            }catch (Exception ex){
+                System.out.println(ex.getMessage());
+            }
+        }else{
+            FileInputStream inputStream = null;
+            Workbook workbook = null;
+            Sheet sheet = null;
+            try{
+                inputStream = new FileInputStream(path);
+                workbook = WorkbookFactory.create(inputStream);
+                sheet = workbook.getSheetAt(0);
+            }catch (Exception ex){
+                System.out.println(ex.getMessage());
+            }
+            Row row = sheet.createRow(sheet.getPhysicalNumberOfRows());
+            Cell cell = row.createCell(0);
+            cell.setCellValue(testResult);
+
+            try{
+                inputStream.close();
+                FileOutputStream outputStream = new FileOutputStream(path);
+                workbook.write(outputStream);
+                workbook.close();
+                outputStream.close();
+            }catch (Exception ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
+
